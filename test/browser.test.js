d76da47269a05f561b11e2fef711acb8de84f9b3
Ian Walter <public@iankwalter.com>
2019-02-06 14:51:15 -0500

    Fixing test names

--- test/browser.test.js
+++ test/browser.test.js
@@ -3,6 +3,6 @@ import puppeteerHelper from '@ianwalter/puppeteer-helper'
 
 const withPage = puppeteerHelper(['./dist/supports-date-input.iife.js'])
 
-test('supportsDateInput returns true for Chrome', withPage, async (t, page) => {
+test(`returns true for Puppeteer's Chrome`, withPage, async (t, page) => {
   t.true(await page.evaluate(() => supportsDateInput()))
 })
