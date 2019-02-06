1107a0bd2139c3a4a72ec27c5a41fb6093e6b8c9
Ian Walter <public@iankwalter.com>
2019-02-06 13:01:10 -0500

    Adding uaSupportsDateInput

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-export default function supportsDateInput () {
+import Bowser from 'bowser'
+
+function supportsDateInput () {
   // Create an invlid date that will be used to test input[type=date] support.
   const notADateValue = 'not-a-date'
 
@@ -13,3 +15,19 @@ export default function supportsDateInput () {
   // and, by the same token, supports input[type=date].
   return input.value !== notADateValue
 }
+
+function uaSupportsDateInput (ua) {
+  const browser = Bowser.getParser(ua)
+  return !!browser.satisfies({
+    firefox: '>=57',
+    chrome: '>=20',
+    'microsoft edge': '>=12',
+    opera: '>=11',
+    mobile: {
+      safari: '>=5',
+      'android browser': '>0'
+    }
+  })
+}
+
+export { supportsDateInput, uaSupportsDateInput }
--- test.js
+++ test.js
@@ -1,4 +1,49 @@
 import test from 'ava'
-// import supportsDateInput from '.'
+// import puppeteerHelper from '@ianwalter/puppeteer-helper'
+import { uaSupportsDateInput } from '.'
 
-test('supportsDateInput', t => t.pass('TODO:'))
+// const withPage = puppeteerHelper(['./dist/supports-date-input.iife.js'])
+
+// test('supportsDateInput', t => t.pass('TODO:'))
+
+test('uaSupportsDateInput returns true for Firefox 65', t => {
+  t.true(uaSupportsDateInput(
+    'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:65.0) Gecko/20100101 Firefox/65.0'
+  ))
+})
+
+test('uaSupportsDateInput returns true for Chrome 72', t => {
+  t.true(uaSupportsDateInput(
+    'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.81 Safari/537.36'
+  ))
+})
+
+test('uaSupportsDateInput returns true for Edge 14', t => {
+  t.true(uaSupportsDateInput(
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.79 Safari/537.36 Edge/14.14393'
+  ))
+})
+
+test('uaSupportsDateInput returns true for Opera 12', t => {
+  t.true(uaSupportsDateInput(
+    'Opera/9.80 (Windows NT 6.1; WOW64) Presto/2.12.388 Version/12.18'
+  ))
+})
+
+test('uaSupportsDateInput returns true for iOS Safari 11', t => {
+  t.true(uaSupportsDateInput(
+    'Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Mobile/15A356 Safari/604.1'
+  ))
+})
+
+test('uaSupportsDateInput returns true for Android Browser 4', t => {
+  t.true(uaSupportsDateInput(
+    'Mozilla/5.0 (Linux; U; Android 2.2) AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile Safari/533.1'
+  ))
+})
+
+test('uaSupportsDateInput returns true for Android Chrome 63', t => {
+  t.true(uaSupportsDateInput(
+    'Mozilla/5.0 (Linux; Android 6.0.1; SM-G532G Build/MMB29T) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.83 Mobile Safari/537.36'
+  ))
+})
